77f4c3fc14ad68ed08a7ede8ce4d7d70757d8c7e
Ruth McElroy <ruthie_mac_@hotmail.com>
2015-08-30 14:10:30 +0100

    more class info

---
+++ Dublin WalkingTourApp/WebServiceWalkingApp/Models/Audio.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dublin_WalkingTourApp.Models
+{
+    class Audio
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public byte[] File { get; set; }
+    }
+}
---
+++ Dublin WalkingTourApp/WebServiceWalkingApp/Models/Location.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Device.Location;
+//using System.Windows.Devices.Geolocation;
+
+namespace Dublin_WalkingTourApp.Models
+{
+    class Location
+    {
+        [Key]
+        public int ID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public GeoCoordinate Stop { get; set; }
+        public int Order{get;set;}
+        public Audio AudioInfo { get; set; }
+
+
+    }
+}
