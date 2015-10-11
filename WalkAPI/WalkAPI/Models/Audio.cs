f2460769dbf42af919c5c6b67151600c2eaec938
Ruth McElroy <ruthie_mac_@hotmail.com>
2015-10-11 15:27:05 +0100

    without portable location library causing problems

--- WalkAPI/WalkAPI/Models/Audio.cs
+++ WalkAPI/WalkAPI/Models/Audio.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Device.Location;
+//using System.Device.Location;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
--- WalkAPI/WalkAPI/Models/Location.cs
+++ WalkAPI/WalkAPI/Models/Location.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Device.Location;
+//using System.Device.Location;
 using System.ComponentModel.DataAnnotations.Schema;
 
 
@@ -17,7 +17,7 @@ namespace WalkAPI.Models
         public int LocationID { get; set; }
         [Required]
         public string Name { get; set; }
-        public GeoCoordinate Stop { get; set; }
+        //public GeoCoordinate Stop { get; set; }
         public int Order { get; set; }
         [ForeignKey("Audio")]
         public int AudioID { get; set; }
